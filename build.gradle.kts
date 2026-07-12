eb5492c4f212966e81f8e6219240339434f51226
RedJulu <jl@redjulu.de>
2026-07-12 13:05:41 +0200

    merge scythe

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation(platform("com.intellectualsites.bom:bom-newest:1.56"))
@@ -33,7 +33,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21.8")
+        minecraftVersion("1.21.11")
         jvmArgs("-Xms2G", "-Xmx2G")
     }
 
